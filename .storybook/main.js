1b38cce7a05af51b7e65c9c5bf505e0a7ffbb0f7
Italo Paiva <8473382+italopaiva@users.noreply.github.com>
2025-06-20 15:42:09 -0300

    Upgrade Storybook to v9.0.12

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,15 @@
-const path = require('path')
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     {
